958374eebe89198624dae28635e733e4b26ffb1a
Neha <neha.mcs.du.2015@gmail.com>
2021-05-18 18:49:17 +0530

    initial commit

---
+++ LoginPageCustomCurve/LoginPageCustomCurveApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageCustomCurveApp.swift
+//  LoginPageCustomCurve
+//
+//  Created by Neha on 18/05/21.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageCustomCurveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
